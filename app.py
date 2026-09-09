3330a08e6045d8ef39f0e9d485dd0a977a3a4c51
MARY016-BEEP <marykabibi4@gmail.com>
2026-09-09 20:28:46 +0300

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ from auth import (
     login_user
 )
 
-from academics import show_academics
+from academics import (
+    show_academics
     CBC_SUBJECTS,
     calculate_grade,
     calculate_results,
